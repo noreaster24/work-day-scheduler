f713e92c7fdf4485b597985b9e85d83d7da1ce1b
Joe Porrazzo <joepa2624@me.com>
2021-04-18 12:27:31 -0700

    text input function

--- assets/script.js
+++ assets/script.js
@@ -1,17 +1,24 @@
 
 var rightNow = moment();
-
 var tomorrow = moment().add(1, "day").format("dddd, MM-D-YYYY");
 console.log(tomorrow);
 
-var auditTimeBlock = function(hourEl) {
-    console.log(hourEl);
-};
 
-var createAgendaItem (hourEl) {
-    var agendaP = $("<p>").addClass("m-1").text(agendaText);
 
-    auditTimeBlock(hourEl);
-
-    $("#textArea").append(agendaP);
-}
+$("#textArea").on("click", function () {
+    var text = $("<p></p>").text("Text.").trim()
+    $(this).replaceWith(textInput);
+    textInput.trigger("focus");
+    $(".textArea").on("blur", "textarea", function () {
+        var text = $(this)
+            .val()
+            .trim();
+        var status = $(this)
+            .closest(".textArea")
+            .attr("id")
+            .replace("text-", "");
+        var index = $(this)
+            .closest(".textArea")
+            .index();
+    })
+});
